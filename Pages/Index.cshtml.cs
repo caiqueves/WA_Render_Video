57c537b107f649bb66e21625de01e0a071fd483d
Caíque Ferreira S. Neves <caique_neves@virgo.atlantico.net.br>
2021-10-27 16:38:36 -0300

    First Commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace RenderVideoArrayBuffer.Pages
+{
+    public class IndexModel : PageModel
+    {
+      
+    }
+}
